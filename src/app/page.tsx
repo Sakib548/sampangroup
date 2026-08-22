1ec4e5802a1e88e3f3b50d78b477703f14272ae0
Sakib548 <mdsakibhassan548@gmail.com>
2026-08-22 22:30:57 +0600

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <HighwayInnCardStack />
 
       <ConcernsSection3 />
-      {/* <ConcernsSection /> */}
+      <ConcernsSection /> 
       {/* <ContactCTA />*/}
     </main>
   );
